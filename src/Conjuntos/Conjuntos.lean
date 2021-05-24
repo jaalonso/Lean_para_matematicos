e450eb3171396cbb6f204f0388bfd0f60ea9071a
Jose A. Alonso <JoseA.Alonso@gmail.com>
2021-05-24 10:52:50 +0200

    Conjuntos.lean: Demostraciones del ejercicio 12.

--- src/Conjuntos/Conjuntos.lean
+++ src/Conjuntos/Conjuntos.lean
@@ -483,6 +483,45 @@ begin
   ext x,
   simp only [mem_inter_eq],
   split,
+  { intro h,
+    split,
+    { exact h.2, },
+    { exact h.1, }},
+  { intro h,
+    split,
+    { exact h.2, },
+    { exact h.1, }},
+end
+
+-- 2ª demostración
+-- ===============
+
+example : s ∩ t = t ∩ s :=
+begin
+  ext,
+  simp only [mem_inter_eq],
+  exact ⟨λ h, ⟨h.2, h.1⟩,
+         λ h, ⟨h.2, h.1⟩⟩,
+end
+
+-- 3ª demostración
+-- ===============
+
+example : s ∩ t = t ∩ s :=
+begin
+  ext,
+  exact ⟨λ h, ⟨h.2, h.1⟩,
+         λ h, ⟨h.2, h.1⟩⟩,
+end
+
+-- 4ª demostración
+-- ===============
+
+example : s ∩ t = t ∩ s :=
+begin
+  ext x,
+  simp only [mem_inter_eq],
+  split,
   { rintros ⟨xs, xt⟩,
     exact ⟨xt, xs⟩ },
   { rintros ⟨xt, xs⟩,
@@ -495,14 +534,44 @@ end
 -- 2. Se ha usado el lema
 --    + mem_inter_eq x s t : x ∈ s ∩ t = (x ∈ s ∧ x ∈ t)
 
--- 2ª demostración
+-- 5ª demostración
+-- ===============
+
+example : s ∩ t = t ∩ s :=
+begin
+  ext x,
+  exact and.comm,
+end
+
+-- Comentario: Se ha usado el lema
+-- + and.comm : a ∧ b ↔ b ∧ a
+
+-- 6ª demostración
+-- ===============
+
+example : s ∩ t = t ∩ s :=
+ext (λ x, and.comm)
+
+-- 7ª demostración
 -- ===============
 
 example : s ∩ t = t ∩ s :=
 by ext x; simp [and.comm]
 
+-- 8ª demostración
+-- ===============
+
+example : s ∩ t = t ∩ s :=
+inter_comm s t
+
 -- Comentario: Se ha usado el lema
--- + and.comm : a ∧ b ↔ b ∧ a
+-- + inter_comm: a ∩ b = b ∩ a
+
+-- 9ª demostración
+-- ===============
+
+example : s ∩ t = t ∩ s :=
+by finish
 
 -- ---------------------------------------------------------------------
 -- Ejercicio 13. Demostrar que
