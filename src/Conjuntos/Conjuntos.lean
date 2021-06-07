d0bea044ce8cd21ff85f82269974e408c627d661
Jose A. Alonso <JoseA.Alonso@gmail.com>
2021-06-07 17:16:12 +0200

    Conjuntos.lean: Ejercicio 47

--- src/Conjuntos/Conjuntos.lean
+++ src/Conjuntos/Conjuntos.lean
@@ -1988,8 +1988,118 @@ by tauto
 --    f ⁻¹' (u ∪ v) = f ⁻¹' u ∪ f ⁻¹' v
 -- ----------------------------------------------------------------------
 
+-- 1ª demostración
+-- ===============
+
 example : f ⁻¹' (u ∪ v) = f ⁻¹' u ∪ f ⁻¹' v :=
-by ext x; refl
+begin
+  ext x,
+  split,
+  { intros h,
+    rw mem_preimage at h,
+    cases h with fxu fxv,
+    { left,
+      apply mem_preimage.mpr,
+      exact fxu, },
+    { right,
+      apply mem_preimage.mpr,
+      exact fxv, }},
+  { intro h,
+    rw mem_preimage,
+    cases h with xfu xfv,
+    { rw mem_preimage at xfu,
+      left,
+      exact xfu, },
+    { rw mem_preimage at xfv,
+      right,
+      exact xfv, }},
+end
+
+-- 2ª demostración
+-- ===============
+
+example : f ⁻¹' (u ∪ v) = f ⁻¹' u ∪ f ⁻¹' v :=
+begin
+  ext x,
+  split,
+  { intros h,
+    cases h with fxu fxv,
+    { left,
+      exact fxu, },
+    { right,
+      exact fxv, }},
+  { intro h,
+    cases h with xfu xfv,
+    { left,
+      exact xfu, },
+    { right,
+      exact xfv, }},
+end
+
+-- 3ª demostración
+-- ===============
+
+example : f ⁻¹' (u ∪ v) = f ⁻¹' u ∪ f ⁻¹' v :=
+begin
+  ext x,
+  split,
+  { rintro (fxu | fxv),
+    { exact or.inl fxu, },
+    { exact or.inr fxv, }},
+  { rintro (xfu | xfv),
+    { exact or.inl xfu, },
+    { exact or.inr xfv, }},
+end
+
+-- 4ª demostración
+-- ===============
+
+example : f ⁻¹' (u ∪ v) = f ⁻¹' u ∪ f ⁻¹' v :=
+begin
+  ext x,
+  split,
+  { finish, },
+  { finish, } ,
+end
+
+-- 5ª demostración
+-- ===============
+
+example : f ⁻¹' (u ∪ v) = f ⁻¹' u ∪ f ⁻¹' v :=
+begin
+  ext x,
+  finish,
+end
+
+-- 6ª demostración
+-- ===============
+
+example : f ⁻¹' (u ∪ v) = f ⁻¹' u ∪ f ⁻¹' v :=
+by ext; finish
+
+-- 7ª demostración
+-- ===============
+
+example : f ⁻¹' (u ∪ v) = f ⁻¹' u ∪ f ⁻¹' v :=
+by ext; refl
+
+-- 8ª demostración
+-- ===============
+
+example : f ⁻¹' (u ∪ v) = f ⁻¹' u ∪ f ⁻¹' v :=
+rfl
+
+-- 9ª demostración
+-- ===============
+
+example : f ⁻¹' (u ∪ v) = f ⁻¹' u ∪ f ⁻¹' v :=
+preimage_union
+
+-- 10ª demostración
+-- ===============
+
+example : f ⁻¹' (u ∪ v) = f ⁻¹' u ∪ f ⁻¹' v :=
+by simp
 
 -- ---------------------------------------------------------------------
 -- Ejercicio 48. Demostrar que
