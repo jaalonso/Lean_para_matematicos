0f9f6cc776c69479c0347d42580fc1e5698806a1
Jose A. Alonso <JoseA.Alonso@gmail.com>
2021-06-15 12:59:30 +0200

    Conjuntos.lean: Ejercicio 56

--- src/Conjuntos/Conjuntos.lean
+++ src/Conjuntos/Conjuntos.lean
@@ -2644,16 +2644,61 @@ union_preimage_subset s u f
 --    f '' (⋃ i, A i) = ⋃ i, f '' A i
 -- ----------------------------------------------------------------------
 
+-- 1ª demostración
+-- ===============
+
+example : f '' (⋃ i, A i) = ⋃ i, f '' A i :=
+begin
+  ext y,
+  split,
+  { intro hy,
+    rw mem_image at hy,
+    cases hy with x hx,
+    cases hx with xUA fxy,
+    rw mem_Union at xUA,
+    cases xUA with i xAi,
+    rw mem_Union,
+    use i,
+    rw ← fxy,
+    apply mem_image_of_mem,
+    exact xAi, },
+  { intro hy,
+    rw mem_Union at hy,
+    cases hy with i yAi,
+    cases yAi with x hx,
+    cases hx with xAi fxy,
+    rw ← fxy,
+    apply mem_image_of_mem,
+    rw mem_Union,
+    use i,
+    exact xAi, },
+end
+
+-- 2ª demostración
+-- ===============
+
 example : f '' (⋃ i, A i) = ⋃ i, f '' A i :=
 begin
   ext y,
   simp,
   split,
-  { rintros ⟨x, ⟨i, xAi⟩, fxeq⟩,
-    use [i, x, xAi, fxeq] },
-  { rintros ⟨i, x, xAi, fxeq⟩,
-    exact ⟨x, ⟨i, xAi⟩, fxeq⟩ },
+  { rintros ⟨x, ⟨i, xAi⟩, fxy⟩,
+    use [i, x, xAi, fxy] },
+  { rintros ⟨i, x, xAi, fxy⟩,
+    exact ⟨x, ⟨i, xAi⟩, fxy⟩ },
 end
+
+-- 3ª demostración
+-- ===============
+
+example : f '' (⋃ i, A i) = ⋃ i, f '' A i :=
+by tidy
+
+-- 4ª demostración
+-- ===============
+
+example : f '' (⋃ i, A i) = ⋃ i, f '' A i :=
+image_Union
 
 -- ---------------------------------------------------------------------
 -- Ejercicio 57. Demostrar que
