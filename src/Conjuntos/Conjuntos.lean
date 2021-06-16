bdc7f4a2e80317611c2c264eb79f20436a3ee2d0
Jose A. Alonso <JoseA.Alonso@gmail.com>
2021-06-16 17:48:33 +0200

    Conjuntos.lean: Ejercicio 59

--- src/Conjuntos/Conjuntos.lean
+++ src/Conjuntos/Conjuntos.lean
@@ -2805,8 +2805,42 @@ end
 --    f ⁻¹' (⋃ i, B i) = ⋃ i, f ⁻¹' (B i)
 -- ----------------------------------------------------------------------
 
+-- 1ª demostración
+-- ===============
+
 example : f ⁻¹' (⋃ i, B i) = ⋃ i, f ⁻¹' (B i) :=
-by { ext x, simp }
+begin
+  ext x,
+  split,
+  { intro hx,
+    rw mem_preimage at hx,
+    rw mem_Union at hx,
+    cases hx with i fxBi,
+    rw mem_Union,
+    use i,
+    apply mem_preimage.mpr,
+    exact fxBi, },
+  { intro hx,
+    rw mem_preimage,
+    rw mem_Union,
+    rw mem_Union at hx,
+    cases hx with i xBi,
+    use i,
+    rw mem_preimage at xBi,
+    exact xBi, },
+end
+
+-- 2ª demostración
+-- ===============
+
+example : f ⁻¹' (⋃ i, B i) = ⋃ i, f ⁻¹' (B i) :=
+preimage_Union
+
+-- 3ª demostración
+-- ===============
+
+example : f ⁻¹' (⋃ i, B i) = ⋃ i, f ⁻¹' (B i) :=
+by  simp
 
 -- ---------------------------------------------------------------------
 -- Ejercicio 60. Demostrar que
