115dc50e042737353038d17e9c8904e5d3c9e656
Jose A. Alonso <JoseA.Alonso@gmail.com>
2021-06-18 11:23:32 +0200

    Conjuntos.lean: Ejercicio 60

--- src/Conjuntos/Conjuntos.lean
+++ src/Conjuntos/Conjuntos.lean
@@ -2840,15 +2840,63 @@ preimage_Union
 -- ===============
 
 example : f ⁻¹' (⋃ i, B i) = ⋃ i, f ⁻¹' (B i) :=
-by  simp
+by simp
 
 -- ---------------------------------------------------------------------
 -- Ejercicio 60. Demostrar que
 --    f ⁻¹' (⋂ i, B i) = ⋂ i, f ⁻¹' (B i)
 -- ----------------------------------------------------------------------
 
+-- 1ª demostración
+-- ===============
+
 example : f ⁻¹' (⋂ i, B i) = ⋂ i, f ⁻¹' (B i) :=
-by { ext x, simp }
+begin
+  ext x,
+  split,
+  { intro hx,
+    apply mem_Inter_of_mem,
+    intro i,
+    rw mem_preimage,
+    rw mem_preimage at hx,
+    rw mem_Inter at hx,
+    exact hx i, },
+  { intro hx,
+    rw mem_preimage,
+    rw mem_Inter,
+    intro i,
+    rw ← mem_preimage,
+    rw mem_Inter at hx,
+    exact hx i, },
+end
+
+-- 2ª demostración
+-- ===============
+
+example : f ⁻¹' (⋂ i, B i) = ⋂ i, f ⁻¹' (B i) :=
+begin
+  ext x,
+  calc  (x ∈ f ⁻¹' ⋂ (i : I), B i)
+      ↔ f x ∈ ⋂ (i : I), B i       : mem_preimage
+  ... ↔ (∀ i : I, f x ∈ B i)       : mem_Inter
+  ... ↔ (∀ i : I, x ∈ f ⁻¹' B i)   : iff_of_eq rfl
+  ... ↔ x ∈ ⋂ (i : I), f ⁻¹' B i   : mem_Inter.symm,
+end
+
+-- 3ª demostración
+-- ===============
+
+example : f ⁻¹' (⋂ i, B i) = ⋂ i, f ⁻¹' (B i) :=
+begin
+  ext x,
+  simp,
+end
+
+-- 4ª demostración
+-- ===============
+
+example : f ⁻¹' (⋂ i, B i) = ⋂ i, f ⁻¹' (B i) :=
+by { ext, simp }
 
 -- ---------------------------------------------------------------------
 -- Ejercicio 61. Demostrar el teorema de Cantor:
